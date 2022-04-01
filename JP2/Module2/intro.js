81c0c5d0db67ccb4cd571627a59093daac943397
ambidexter15 <akanksha.gupta@pepcoding.com>
2022-04-01 21:38:41 +0530

    module2

---
+++ JP2/Module2/intro.js
@@ -0,0 +1,21 @@
+//parampara
+console.log("Hello World");
+console.log("Hello Pepcoder");
+
+//Declaraing a variable
+//let, const, var 
+
+let a;
+a = "Akanksha"; //String
+console.log("My name is", a);
+
+let b = 10;    //number
+console.log("I am number:", b);
+
+let c;
+c = true;    //boolean
+console.log("I am boolean value which says:",c);
+
+let d = null;   //null
+console.log("Hey I am the coolest:", d);
+
