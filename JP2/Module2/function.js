c648e7b6893398e2c7b1666cbe46807a58a35bb5
ambidexter15 <akanksha.gupta@pepcoding.com>
2022-04-01 22:34:44 +0530

    functions part 2

--- JP2/Module2/function.js
+++ JP2/Module2/function.js
@@ -28,3 +28,37 @@ function f3(a,b)
 let ans = f3(21,5);
 console.log(ans);
 console.log(f3(30,5));
+
+//4. Immediately invoked functional expressions (IIFE)
+(function f4(){
+    console.log("I am an IIFE")
+   })();
+
+//5. Immediately invoked functional expressions (IIFE) with parameter
+
+(function f5(a=2,b=4){
+    console.log("I am an IIFE with parameters", a*b)
+})();
+
+
+//6. function as a variable 
+
+let f6 = function f5()
+{
+    console.log("Hello I am variable function")
+}
+
+console.log(f6);
+console.log(f6());
+
+let random;
+console.log("random:",random);
+
+let f7 = function f5(x,y)
+{
+    console.log("Hey I am function 7")
+    return x +y;
+}
+
+console.log("f7 : ",f7(10,20));
+
