16bb157dc052f8006cfa0f7e024d000d6c2f0f02
ambidexter15 <akanksha.gupta@pepcoding.com>
2022-04-04 23:07:57 +0530

    let and const variables

---
+++ JP2/Module2/let_const.js
@@ -0,0 +1,23 @@
+//let keyword
+// 1. redeclare - not allowed
+// 2. reassign - allowed
+// let b = "hello";
+// console.log(b);
+// console.log(a);
+// let a;
+// console.log(a);
+// a =10;
+// console.log(a);
+// a = 20;
+// console.log(a);
+// a = "hello"
+// console.log(a);
+
+//const keyword 
+// 1. redeclaration not allowed
+// 2. reassignment not allowed
+
+const c =20;
+console.log(c);
+// c =20;
+console.log(c);
