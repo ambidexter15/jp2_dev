0436618e10c12eb9f508700686c986e1eb4c4cda
ambidexter15 <akanksha.gupta@pepcoding.com>
2022-04-06 22:06:47 +0530

    child_process

---
+++ JP2/Module2/node/child_process.js
@@ -0,0 +1,9 @@
+let cp = require("child_process");
+
+// cp.execSync("calc");
+// cp.execSync("start chrome");
+console.log("Open pepcoding site");
+cp.execSync("start chrome https://pepcoding.com/");
+console.log("Opened pepcoding site");
+
+
